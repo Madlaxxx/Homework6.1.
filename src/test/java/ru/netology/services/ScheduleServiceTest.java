8f9e772a6764ba9ee98e5793536c43ec76217da3
Мария <masha2355@gmail.com>
2023-07-17 18:58:02 +1000

    Csv file

--- src/test/java/ru/netology/services/ScheduleServiceTest.java
+++ src/test/java/ru/netology/services/ScheduleServiceTest.java
@@ -2,29 +2,32 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class ScheduleServiceTest {
     ScheduleService service = new ScheduleService();
 
-    @Test
-    public void ScheduleCalc() {
-        int expected = 3;
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/schedule.csv")
+    public void ScheduleCalc(int expected, int income, int expenses, int threshold) {
+        //int expected = 3;
+        //int income = 10_000;
+        //int expenses = 3_000;
+        //int threshold = 20_000;
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(actual, expected);
     }
 
-    @Test
-    public void ScheduleOrCalc() {
-        int expected = 2;
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int actual = service.calculate(income, expenses, threshold);
+    //@Test
+    // public void ScheduleOrCalc() {
+    //int expected = 2;
+    //int income = 100_000;
+    //int expenses = 60_000;
+    //int threshold = 150_000;
+    //int actual = service.calculate(income, expenses, threshold);
 
-        Assertions.assertEquals(actual, expected);
-    }
+    //Assertions.assertEquals(actual, expected);
+    //}
 }
